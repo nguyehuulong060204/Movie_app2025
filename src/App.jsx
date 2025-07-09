351c2a451c503031d5782958beda3904447f6a37
minhai06022004 <ln367812@gmail.com>
2025-07-09 14:48:03 +0700

    Khởi tạo setup dự án Movie-app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movie</span> you'll Enjoy Without The Hassle</h1>
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default App;
